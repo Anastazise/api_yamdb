dab169b15660f838641151037349f5e01d34d54f
Evgeniy Kosukha <kosukhaevgeniy@gmail.com>
2023-02-14 19:37:02 -0300

    added review, comments models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 User = get_user_model()
 
@@ -48,3 +49,64 @@ class Title(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    text = models.TextField(
+        verbose_name='Текст отзыва',
+        max_length=256
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    score = models.SmallIntegerField(
+        default=1,
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    class Meta:
+        unique_together = ('author', 'title')
+
+    def __str__(self):
+        return self.text[:10]
+
+
+class Comment(models.Model):
+    text = models.TextField(
+        verbose_name='Текст коментария',
+        max_length=256
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Произведение'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.text[:10]
