1d1c69981562cff012bf0cb8147ae42c29cdb3d3
Evgeniy Kosukha <kosukhaevgeniy@gmail.com>
2023-02-15 10:31:36 -0300

    minor changes in permission.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -6,5 +6,4 @@ class IsAuthorOrStaff(permissions.BasePermission):
     permission to allow owners, admins and moderators to edit and delete objects.
     """
     def has_object_permission(self, request, view, obj):
-        print(request.user)
         return obj.author == request.user or request.user.is_staff
