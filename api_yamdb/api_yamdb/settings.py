004211ad2d0a151719656a76bae84f133f75fa41
Evgeniy Kosukha <kosukhaevgeniy@gmail.com>
2023-02-15 07:17:05 -0300

    api settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,12 +17,14 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'rest_framework',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api_yamdb',
     'reviews',
+    'api',
 ]
 
 MIDDLEWARE = [
