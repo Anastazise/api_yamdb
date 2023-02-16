e5591427781235ad627666a20bd035fb84d73ef9
Anastazise <anastazidevilfor@yandex.ru>
2023-02-17 01:51:20 +0300

    add user model

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_yamdb',
     'reviews',
     'api',
 ]
@@ -110,3 +109,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
+
+AUTH_USER_MODEL = 'reviews.User'
